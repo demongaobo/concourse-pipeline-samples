7120741ac3d25dbd48abd49ce754b4cd369322a9
demongaobo <414052702@qq.com>
2020-07-21 16:47:54 +0800

    Update index.js

--- blue-green-app-deployment/bgd-app/index.js
+++ blue-green-app-deployment/bgd-app/index.js
@@ -35,7 +35,7 @@ app.all('*', function(req, res) {   // serve all other requests
   res.write("<p><img src='./images/"+icon_name+"'></p>");
   res.write("<hr>");
   res.write("<p><b>Application name:</b> "+ app_obj.application_name+"</p>");
-  res.write("<p><b>Application version:</b> "+ app_obj.application_version+"test</p>");
+  res.write("<p><b>Application version:</b> "+ app_obj.application_version+"</p>");
   res.write("<p><b>Application URIs:</b> "+ app_obj.application_uris+"</p>");
   res.write("<hr><p><b>VCAP_APPLICATION:</b> "+ JSON.stringify(app_obj,null,'\t')+"</p>");
   res.write("<hr><p>Current time: "+new Date().toString()+"</p><hr/>");
